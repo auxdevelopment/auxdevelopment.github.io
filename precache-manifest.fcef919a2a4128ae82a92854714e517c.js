a2be123316039744566a63706f8f5428f4376a60
auxdevelopment <benjamin.au.99@gmx.at>
2021-03-22 23:01:52 +0100

    Add vue

---
+++ precache-manifest.fcef919a2a4128ae82a92854714e517c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec046f82bdcab4f4eff",
+    "url": "/css/app.38a92a01.css"
+  },
+  {
+    "revision": "b4e8eb6259955069e984773827a9b21f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cec046f82bdcab4f4eff",
+    "url": "/js/app.f66976f9.js"
+  },
+  {
+    "revision": "a3858641eda518dc3ac0",
+    "url": "/js/chunk-vendors.a1234ddb.js"
+  },
+  {
+    "revision": "b1522009f3fb4b8b45206e76393903f7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.fcef919a2a4128ae82a92854714e517c.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "gratitude-reporter-app"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
